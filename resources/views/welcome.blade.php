870c5919080ef9c1302ced472de82a1df6dc37ac
Julien Flusin <julienflusin3@gmail.com>
2021-02-03 14:43:23 +0100

    Correction faute d'orthographes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -82,7 +82,7 @@
                 <img src="" class="image" style="width:100%">
                 <div class="explication center">
                     <div class="text">
-                        <span style="font-weight: bold;">Bienvenue sur le site du bureau des élève du lycée Sainte Famille Saintonge. Crée en 2018, ce bureau regroupe trois entités détaillées ci-dessus : le Bureau Des Actions (BDA), le Bureau des Sports (BDS), le Bureau de la culture (BDC) ainsi que le Pôle Communication.</span>
+                        <span style="font-weight: bold;">Bienvenue sur le site du bureau des élèves du lycée Sainte Famille Saintonge. Créé en 2018, ce bureau regroupe trois entités détaillées ci-dessus : le Bureau Des Actions (BDA), le Bureau des Sports (BDS), le Bureau de la culture (BDC) ainsi que le Pôle Communication.</span>
                     </div>
                 </div>
             </div>
