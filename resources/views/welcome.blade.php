94bf36d8572b7257b6570fb0946ff1d02883e3c5
Julien Flusin <julienflusin3@gmail.com>
2021-02-06 17:27:23 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <title>BDE Saintonge </title>
 
-    <!-- Fichier Locaux CSS hey-->
+    <!-- Fichier Locaux CSS-->
     <link rel="stylesheet" href="{{asset('css/reboot.css')}}">
     <link rel="stylesheet" href="{{asset('css/responsive.css')}}">
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
