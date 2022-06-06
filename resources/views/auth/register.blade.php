4b46ac862d9109336f059fd73ac2283bc1af8827
Clément LO-CASCIO <49551394+ClemLcs@users.noreply.github.com>
2022-06-06 22:39:10 +0200

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -21,35 +21,6 @@
                         <input type="text" class="form-control" name="name" id="name" value="{{ old('name') }}">
                     </div>
                     <div class="form-group">
-                        {!! $errors->first('classe', '<div class="alert alert-warning" role="alert">:message</div>')!!}
-                        <label for="class">Votre classe:</label>
-                        <select  class="form-control" name="class" id="class">
-                            <option name="seconde_gt_a">Seconde GT A</option>
-                            <option name="seconde_gt_b">Seconde GT B</option>
-                            <option name="seconde_pro_tisec">Seconde PRO TISEC</option>
-                            <option name="seconde_pro_melec">Seconde PRO MELEC</option>
-                            <option name="seconde_pro_tbee">Seconde PRO TBEE</option>
-                            <option name="seconde_pro_mbc">Seconde PRO MBC</option>
-                            <option name="premiere_s">Première SSI</option>
-                            <option name="premiere_sti_a">Première STI A</option>
-                            <option name="premiere_sti_b">Première STI B</option>
-                            <option name="premiere_sti_c">Première STI C</option>
-                            <option name="premiere_pro_tisec">Premiere PRO TISEC</option>
-                            <option name="premiere_pro_melec">Premiere PRO MELEC</option>
-                            <option name="premiere_pro_tbee">Premiere PRO TBEE</option>
-                            <option name="premiere_pro_mbc">Premiere PRO MBC</option>
-                            <option name="premiere_pro_macon">Premiere PRO Maçon</option>
-                            <option name="premiere_pro_ifca">Premiere PRO IFCA</option>
-                            <option name="terminal_ssi">Terminal SSI</option>
-                            <option name="terminal_sti_a">Terminal STI A</option>
-                            <option name="terminal_sti_b">Terminal STI B</option>
-                            <option name="terminal_sti_c">Terminal STI C</option>
-                            <option name="terminale_pro_tisec">Terminale PRO TISEC</option>
-                            <option name="terminale_pro_melec">Terminale PRO MELEC</option>
-                            <option name="terminale_pro_tbee">Terminale PRO TBEE</option>
-                        </select>
-                    </div>
-                    <div class="form-group">
                         {!! $errors->first('email', '<div class="alert alert-warning" role="alert">:message</div>')!!}
                         <label for="email">Votre email:</label>
                         <input type="email" class="form-control" name="email" value="{{ old('email') }}" id="email">
